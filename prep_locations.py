cc39b04f2697d6249f7f5f5634ae23cd1edde0c0
Jason Heppler <jason.heppler@gmail.com>
2023-09-06 14:17:30 -0500

    Include location data from the postmarks

--- prep_locations.py
+++ prep_locations.py
@@ -10,7 +10,24 @@ columns_to_extract = ['Addressee Town/City', 'Addressee Province/State', 'Addres
 data = pd.read_excel(excel_file, usecols=columns_to_extract, sheet_name="Box 3 Folders XVI-XXII")
 data.columns = ["city", "state", "country"]
 
+# Now we also need to grab the data from the 'Postmark 1 Location' and 'Postmark 2 Location'
+# and add them to the city column.
+postmark_columns = ['Postmark 1 Location', 'Postmark 2 Location']
+
+postmark_data = pd.read_excel(excel_file, usecols=postmark_columns, sheet_name="Box 3 Folders XVI-XXII")
+# Recombine so that the two location columns are a single column of data
+postmark_data = pd.concat([postmark_data['Postmark 1 Location'], postmark_data['Postmark 2 Location']])
+# Now structure the data. We want to add the postmark data to the city column
+postmark_data = postmark_data.to_frame()
+postmark_data.columns = ["city"]
+postmark_data["state"] = ""
+postmark_data["country"] = ""
+
+# Now we need to combine the two dataframes
+data = pd.concat([data, postmark_data])
+
 unique_data = data.drop_duplicates()
+uniqque_data = data.dropna()
 
 output_dir = "data"
 os.makedirs(output_dir, exist_ok=True)
